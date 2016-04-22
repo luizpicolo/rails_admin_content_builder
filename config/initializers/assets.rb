16c88b8f270a01500e245bcaf2cbbc32d52b5962
Luiz Picolo <luizpicolo@gmail.com>
2016-04-22 10:05:06 -0400

    adicionada imagens para lateral e default

---
+++ config/initializers/assets.rb
@@ -0,0 +1,5 @@
+Rails.application.config.assets.precompile += %w( snippet-text.png )
+Rails.application.config.assets.precompile += %w( snippet-image.png )
+Rails.application.config.assets.precompile += %w( snippet-image-text.png )
+Rails.application.config.assets.precompile += %w( snippet-text-image.png )
+Rails.application.config.assets.precompile += %w( default.jpg )
