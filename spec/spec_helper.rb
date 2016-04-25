dafb101abf468915fd39e62c0244e4410ca8f33d
Luiz Picolo <luizpicolo@gmail.com>
2016-04-25 14:42:51 -0400

    update

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,15 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+ENV["RAILS_ENV"] = "test"
+
+require 'rails'
+require 'action_controller'
+require 'action_controller/base'
+require 'action_controller/test_case'
+require 'rspec/rails'
 require 'rails_admin_content_builder'
+
+class ApplicationController < ActionController::Base; end
+
+require_relative '../app/controllers/rails_admin_content_builder/content_builder_controller'
 
 RSpec.configure do |config|
 end
