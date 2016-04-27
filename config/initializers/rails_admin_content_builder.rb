b1fe068dfc85a6c3e32a5e75fbded2982ee830c2
Luiz Picolo <luizpicolo@gmail.com>
2016-04-27 09:12:55 -0400

    Added feature for create

--- config/initializers/rails_admin_content_builder.rb
+++ config/initializers/rails_admin_content_builder.rb
@@ -1,27 +1,25 @@
-unless Rails.env.test?
-  RailsAdmin.config do |config|
-    config.actions do
-      content_builder do
-        only ['RailsAdminContentBuilder::ContentBuilder']
-      end
+RailsAdmin.config do |config|
+  config.actions do
+    content_builder do
+      only ['RailsAdminContentBuilder::ContentBuilder']
+    end
 
-      config.model 'RailsAdminContentBuilder::ContentBuilder' do
-        list do
-          field :id
-          field :title
-          field :date_publish
-          field :status
-        end
-        edit do
-          field :title
-          field :date_publish
-          field :content_builder_relateds
-        end
+    config.model 'RailsAdminContentBuilder::ContentBuilder' do
+      list do
+        field :id
+        field :title
+        field :date_publish
+        field :status
       end
+      edit do
+        field :title
+        field :date_publish
+        field :content_builder_relateds
+      end
+    end
 
-      config.model 'RailsAdminContentBuilder::ContentBuilderImage' do
-        visible false
-      end
+    config.model 'RailsAdminContentBuilder::ContentBuilderImage' do
+      visible false
     end
   end
 end
