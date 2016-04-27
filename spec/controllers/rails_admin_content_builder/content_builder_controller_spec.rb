c13f321652994c8c1b500ff8e78a3efadc349ab3
Luiz Picolo <luizpicolo@gmail.com>
2016-04-27 13:43:35 -0400

    Added gem Friendly_id

--- spec/controllers/rails_admin_content_builder/content_builder_controller_spec.rb
+++ spec/controllers/rails_admin_content_builder/content_builder_controller_spec.rb
@@ -5,7 +5,7 @@ describe RailsAdminContentBuilder::ContentBuilderController, type: :controller d
   describe 'PUT #create_images' do
     it "Should return valid json" do
       content_builder = FactoryGirl.create :content_builder
-      put :create_images, id: content_builder,
+      put :create_images, id: content_builder.id,
           content_builder_image: Rack::Test::UploadedFile.new(File.join('spec', 'fixtures', 'assets', 'example.jpg'))
 
       json = JSON.parse(response.body)
