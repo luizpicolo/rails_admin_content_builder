acca182516e269ff94daa75fa2562158673b4a83
Luiz Picolo <luizpicolo@gmail.com>
2016-04-27 16:52:23 -0400

    Implemented method content_sanitized

--- app/models/rails_admin_content_builder/content_builder.rb
+++ app/models/rails_admin_content_builder/content_builder.rb
@@ -7,5 +7,13 @@ module RailsAdminContentBuilder
 
     validates :title, :date_publish, presence: true
     has_many :content_builder_images, inverse_of: :content_builder
+
+    def content_sanitized
+      # sanitize(
+      #   self.content,
+      #   tags: %w(div p img figure figcaption),
+      #   attributes: %w(src class)).html_safe
+      self.content.html_safe
+    end
   end
 end
