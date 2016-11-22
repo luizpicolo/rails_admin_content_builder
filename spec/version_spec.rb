7f36c06eebc08cfc601375533bb9415e64fcb8a9
luizpicolo <luizpicolo@gmail.com>
2016-11-22 00:27:19 -0300

    Bump version 1.0.6

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe RailsAdminContentBuilder do
   it 'has a version number' do
-    expect(RailsAdminContentBuilder::VERSION).to eq('1.0.5')
+    expect(RailsAdminContentBuilder::VERSION).to eq('1.0.6')
   end
 end
